a389efa7c955a156a2eb650f15d94f31ee8c5ed2
Umair Ul Hassan <mrumairhassan@outlook.com>
2025-08-28 20:42:48 +0500

    code clean

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import "./App.css"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Header from "./components/Header"
-import Hero from "./components/Hero"
-import NotFoundSection from "./components/NotFoundSection"
-import Footer from "./components/Footer"
-import BookingPage from "./components/BookingPage" 
-import AboutPage from "./components/AboutPage"
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import NotFoundSection from "./components/NotFoundSection";
+import Footer from "./components/Footer";
+import BookingPage from "./components/BookingPage";
+import AboutPage from "./components/AboutPage";
 
 function App() {
   return (
@@ -13,20 +13,22 @@ function App() {
       <div className="app">
         <Header />
         <Routes>
-          <Route path="/" element={
-            <>
-            {/* <AboutPage /> */}
-              <Hero />
-              <NotFoundSection />
-            </>
-          } />
+          <Route
+            path="/"
+            element={
+              <>
+                <Hero />
+                <NotFoundSection />
+              </>
+            }
+          />
           <Route path="/booking" element={<BookingPage />} />
           <Route path="/about" element={<AboutPage />} />
         </Routes>
         <Footer />
       </div>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
